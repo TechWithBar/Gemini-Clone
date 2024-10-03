74337703fc702c7b59a04ebb9db810b83d1e48e5
Bar Fishilevich <barfishilev@gmail.com>
2024-10-03 10:09:50 +0300

    First Commit
    First Date: 3.10.2024

---
+++ src/config/gemini.js
@@ -0,0 +1,53 @@
+/*
+ * Install the Generative AI SDK
+ *
+ * $ npm install @google/generative-ai
+ */
+
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } from "@google/generative-ai"
+  
+  const apiKey = "AIzaSyAS3eavlVBZDlKndKYtMidcKfJPVDqzHBg";
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash",
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 64,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+  
+  const safetySettings = [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+      threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+    },
+  ];
+
+  async function run(prompt) {
+    const chatSession = model.startChat({
+      generationConfig,
+      safetySettings,
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(prompt);
+    const response = result.response;
+    console.log(response.text());
+    return response.text();
+  }
+  
+  export default run;
